154776a3789ea3d40d7735f06aea1e039ec8c63f
Martha Lambert <marthaklambert@Marthas-MacBook-Pro.local>
2019-09-25 15:18:07 +0100

    /todos and /addtodo endpoints

--- src/router.js
+++ src/router.js
@@ -20,7 +20,47 @@ const handler = (request, response) => {
     endpoint = request.url;
     if (endpoint === "/") {
         handleHome(request, response);
+    } else if (endpoint === "/todos") {
+        const filePath = path.join(__dirname, "todos.json");
+        fs.readFile(filePath, (error, file) => {
+            if (error) {
+                console.log(error);
+                response.writeHead(404, { "Content-Type": "text/html" });
+                response.end("<h1>404 Not Found</h1>");
+            } else {
+                console.log("okay");
+
+                response.writeHead(200, { "Content-Type": "application/json" });
+                response.end(file);
+                console.log(JSON.parse(file));
+                return file;
+            }
+        });
+    } else if (endpoint === "/addtodo") {
+        const filePath = path.join(__dirname, "todos.json");
+        fs.readFile(filePath, (error, file) => {
+            if (error) {
+                console.log(error);
+                response.writeHead(404, { "Content-Type": "text/html" });
+                response.end("<h1>404 Not Found</h1>");
+            } else {
+                const parsedTodos = JSON.parse(file);
+                parsedTodos.push({
+                    id: parsedTodos.length + 1,
+                    title: request.headers.title
+                });
+                fs.writeFile(filePath, JSON.stringify(parsedTodos), "utf8", (error) => {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        response.writeHead(201, { "Content-Type": "text/html" });
+                        response.end("New todo added");
+                    }
+                });
+            }
+        });
     }
+
     // else if (endpoint.indexOf("css") !== -1 || endpoint.indexOf("img") !== -1) {
     //     handlePublic(request, response, endpoint);
     // } else if (endpoint === "/create-post") {
