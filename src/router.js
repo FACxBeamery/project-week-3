1d2dc04c4ed25384679accd3672841004bd1880d
João Viana <joaoviana@Joaos-MacBook-Pro.local>
2019-09-27 10:10:58 +0100

    error handling if request headers are empty or if id does not exist in json file

--- src/router.js
+++ src/router.js
@@ -21,80 +21,110 @@ const getTodos = (response, request, error, file) => {
 
 const addTodo = (response, request, error, file, filePath) => {
     let parsedTodos = JSON.parse(file);
-    const newTodo = {
-        id: parsedTodos.length + 1,
-        title: request.headers.title,
-        completed: false,
-        dateCreated: Date.now(),
-        dateEdited: Date.now()
-    };
-    parsedTodos = pf.addToArray(parsedTodos, newTodo);
+    console.log(request.headers.title);
 
-    writeTodoList(
-        filePath,
-        JSON.stringify(parsedTodos),
-        error,
-        response,
-        `todo with id: ${newTodo.id} has been added successfully`
-    );
+    if (!request.headers.title) {
+        response.writeHead(400, { "Content-Type": "text/html" });
+        response.end("Header <title> cannot be empty. Please try again");
+    } else {
+        const newTodo = {
+            id: parsedTodos.length + 1,
+            title: request.headers.title,
+            completed: false,
+            dateCreated: Date.now(),
+            dateEdited: Date.now()
+        };
+        parsedTodos = pf.addToArray(parsedTodos, newTodo);
+
+        writeTodoList(
+            filePath,
+            JSON.stringify(parsedTodos),
+            error,
+            response,
+            `todo with id: ${newTodo.id} has been added successfully`
+        );
+    }
 };
 
 const removeTodo = (response, request, error, file, filePath) => {
     const id = request.headers.id;
     let parsedTodos = JSON.parse(file);
-    parsedTodos = pf.removeFromArray(parsedTodos, id);
+    if (!checkIfIDExists(parsedTodos, id)) {
+        response.writeHead(400, { "Content-Type": "text/html" });
+        response.end("Could not find to do with that id. Please try again");
+    } else {
+        parsedTodos = pf.removeFromArray(parsedTodos, id);
 
-    writeTodoList(
-        filePath,
-        JSON.stringify(parsedTodos),
-        error,
-        response,
-        `todo with id: ${id} has been removed successfully`
-    );
+        writeTodoList(
+            filePath,
+            JSON.stringify(parsedTodos),
+            error,
+            response,
+            `todo with id: ${id} has been removed successfully`
+        );
+    }
 };
 
 const toggleTodoStatus = (response, request, error, file, filePath) => {
     const id = request.headers.id;
     let parsedTodos = JSON.parse(file);
     const newStatus = request.url === "/completetodo";
-    parsedTodos = pf.toggleItemStatus(parsedTodos, id, newStatus, Date.now());
 
-    writeTodoList(
-        filePath,
-        JSON.stringify(parsedTodos),
-        error,
-        response,
-        `todo with id: ${id} has had its status toggled to ${newStatus}`
-    );
+    if (!checkIfIDExists(parsedTodos, id)) {
+        response.writeHead(400, { "Content-Type": "text/html" });
+        response.end("Could not find to do with that id. Please try again");
+    } else {
+        parsedTodos = pf.toggleItemStatus(parsedTodos, id, newStatus, Date.now());
+
+        writeTodoList(
+            filePath,
+            JSON.stringify(parsedTodos),
+            error,
+            response,
+            `todo with id: ${id} has had its status toggled to ${newStatus}`
+        );
+    }
 };
 
 const sortTodos = (response, request, error, file, filePath) => {
     const sortBy = request.headers.sortby;
     let parsedTodos = JSON.parse(file);
     parsedTodos = pf.sortArray(parsedTodos, sortBy);
-    writeTodoList(
-        filePath,
-        JSON.stringify(parsedTodos),
-        error,
-        response,
-        `Todos have been sorted by ${sortBy}`
-    );
+    if (sortBy !== "date" || sortBy !== "latest" || sortBy !== "status") {
+        response.writeHead(400, { "Content-Type": "text/html" });
+        response.end(
+            "Incorrect header value <sortby>. Please try again. E.g. date, latest, or status"
+        );
+    } else {
+        writeTodoList(
+            filePath,
+            JSON.stringify(parsedTodos),
+            error,
+            response,
+            `Todos have been sorted by ${sortBy}`
+        );
+    }
 };
 
 const editTodo = (response, request, error, file, filePath) => {
     const id = request.headers.id;
     const newTitle = request.headers.title;
 
-    let parsedTodos = JSON.parse(file);
-    parsedTodos = pf.editArray(parsedTodos, id, newTitle, date.Now());
+    if (!checkIfIDExists(parsedTodos, id) || !newTitle) {
+        response.writeHead(400, { "Content-Type": "text/html" });
+        response.end("Could not find to do with that id/title header was empty. Please try again");
+    } else {
+        let parsedTodos = JSON.parse(file);
+        parsedTodos = pf.editArray(parsedTodos, id, newTitle, date.Now());
 
-    writeTodoList(
-        filePath,
-        JSON.stringify(parsedTodos),
-        error,
-        response,
-        `Todo with id: ${id} has been edited`
-    );
+        writeTodoList(
+            filePath,
+            JSON.stringify(parsedTodos),
+            error,
+            response,
+            `Todo with id: ${id} has been edited`
+        );
+    }
 };
 
 const writeTodoList = (filePath, jsonTodoList, error, response, message) => {
@@ -113,6 +143,8 @@ const handleHome = (response, request, error, file) => {
     response.writeHead(200, { "Content-Type": "text/html" });
     response.end(file);
 };
+
+const checkIfIDExists = (array, id) => array.some((item) => item.id == id);
 
 module.exports = {
     readTodoList,
